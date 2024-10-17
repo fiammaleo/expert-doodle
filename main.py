2a6268540ab622dd2aa409c9b7f4de43d28ea52d
fiammaleo <fiamma.leo.isa@gmail.com>
2024-10-17 20:53:12 +0000

    m

---
+++ main.py
@@ -0,0 +1,12 @@
+nombre="danayfiamma"
+numero=3
+booleano=True
+booleano=False
+print(numero)
+print(3)
+edadDeDanayFiamma=15
+edadParaVotar=16
+puedeVotar= edadParaVotar<=edadDeDanayFiamma
+if puedeVotar:
+    print("puede votar")
+    
